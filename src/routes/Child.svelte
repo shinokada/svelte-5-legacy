dbdaa6a5025310be9fa9bfd2c42fa560330908aa
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2024-06-30 13:15:59 +0200

    fix: clean up Child

--- src/routes/Child.svelte
+++ src/routes/Child.svelte
@@ -1,18 +1,11 @@
 <script lang="ts">
   import { type TransitionConfig } from 'svelte/transition';
-  import type { Action } from 'svelte/action';
   type TransitionFunc = (node: HTMLElement, params: any) => TransitionConfig;
 	let visible = true;
 
   export let transition: TransitionFunc | undefined = undefined;
   export let params: object = {};
   export let tag: string = 'div';
-  export let role: string | undefined = undefined;
-  export let options: object = {};
-  export let divClass: string = '';
-  export let node: HTMLElement | undefined = undefined;
-  const noop = () => {};
-  export let use: Action<HTMLElement, any> = noop;
 </script>
 
 <label>
@@ -21,7 +14,7 @@
 </label>
 
 {#if visible && transition}
-<svelte:element this={tag} transition:transition={params} use:use={options}  bind:this={node} {role} {...$$restProps} class={divClass} on:click on:mouseenter on:mouseleave on:focusin on:focusout>
+<svelte:element this={tag} transition:transition={params} >
   <slot />
 </svelte:element>
 {/if}
